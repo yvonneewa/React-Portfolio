333d1bafd1cbe09bfda858f94eae7a5eea3cef0b
Yvonne <yowantings@gmail.com>
2024-07-21 16:29:43 -0600

    fixed profolio links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,37 +4,37 @@ import '../index.css';
 const projects = [
   {
     title: 'PWA challenge',
-    imageSrc: './src/images/pwa.png',
+    imageSrc: './src/images/computer1.jpg',
     deployedLink: 'https://pwa-challenge-text-editor.onrender.com/',
     githubLink: 'https://github.com/yvonneewa/PWA-Challenge-Text-Editor'
   },
   {
     title: 'MCV challenge',
-    imageSrc: './src/images/mvc.png',
+    imageSrc: './src/images/computer9.jpg',
     deployedLink: 'https://mvc-challenge-tech-blog-12jv.onrender.com/',
     githubLink: 'https://github.com/yvonneewa/MVC-Challenge-Tech-Blog'
   },
   {
     title: 'NoSql Challenge',
-    imageSrc: './src/images/nosql.png', 
+    imageSrc: './src/images/computer3.jpg',
     VideoWalkthrough: 'https://drive.google.com/file/d/1ArQQsEV22DbpJ7dd4p88QIuqxXj-htSC/view',
     githubLink: 'https://github.com/yvonneewa/NoSQL-Social-Network-API'
   },
   {
     title: 'ORM Challenge',
-    imageSrc: './src/images/orm.png', 
+    imageSrc: './src/images/computer10.jpg',
     VideoWalkthrough: 'https://app.screencastify.com/v3/watch/EPuasD2S821kz0h27OT8',
     githubLink: 'https://github.com/yvonneewa/ORM-Challenge'
   },
   {
-    title: 'Note Taker App',
-    imageSrc: './src/images/notetaker.jpg', 
+    title: 'NoteTaker App',
+    imageSrc: './src/images/computer5.jpg',
     deployedLink: 'https://note-taker-challenge-mt8b.onrender.com/',
     githubLink: 'https://github.com/yvonneewa/Note-Taker-challenge'
   },
   {
     title: 'Taskbar App',
-    imageSrc: './src/images/todo.png', 
+    imageSrc: './src/images/computer6.jpg', 
     deployedLink: 'https://yvonneewa.github.io/taskbar-challenge/',
     githubLink: 'https://github.com/yvonneewa/taskbar-challenge'
   }
@@ -47,10 +47,10 @@ function Portfolio() {
       <div className="projects">
         {projects.map((project, index) => (
           <div key={index} className="project">
-            <a href={project.deployedLink || project.githubLink} target="_blank" rel="noopener noreferrer">
+            {/* <a href={project.deployedLink || project.githubLink} target="_blank" rel="noopener noreferrer">
               <img src={project.imageSrc} alt={project.title} />
-            </a>
-            <div className="project-details">
+            </a> */}
+            <div className="project-details" style ={{backgroundImage:`url(${project.imageSrc})`}}>
               <h3>{project.title}</h3>
               <p>
                 {project.VideoWalkthrough && (
