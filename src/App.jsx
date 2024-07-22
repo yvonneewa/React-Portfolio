d737956eeafb3f993524a6314a4df79ef2c0d85b
Yvonne <yowantings@gmail.com>
2024-07-21 18:07:19 -0600

    fixed some text

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <header className="bg-gray-100 py-4">
         <nav className="container mx-auto flex justify-between items-center">
-          <div className="logo">Yvonne's Blog</div>
+          <div className="logo">Yvonnes Portfolio</div>
           <ul className="flex items-center">
             <li className="mr-6">
               <a href="/" className="text-gray-700 hover:text-gray-900">
