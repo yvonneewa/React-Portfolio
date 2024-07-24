85ad46c273c92eeb1cc24a5386acaf52f0c60141
Yvonne <yowantings@gmail.com>
2024-07-23 21:05:02 -0600

    added a file?

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App';
+
+
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './components/About.jsx';
